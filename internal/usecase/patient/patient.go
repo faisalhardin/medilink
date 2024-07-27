ee423c9d45bff20c0711724d40dc6253aba31ff0
faisalhardin <rangkuti.hardin@gmail.com>
2024-07-27 15:39:25 +0700

    fix(mdl): patient usecase

--- internal/usecase/patient/patient.go
+++ internal/usecase/patient/patient.go
@@ -2,7 +2,6 @@ package patient
 
 import (
 	"context"
-	"database/sql"
 
 	"github.com/pkg/errors"
 
@@ -25,24 +24,14 @@ func NewPatientUC(u *PatientUC) *PatientUC {
 
 func (u *PatientUC) RegisterNewPatient(ctx context.Context, req model.RegisterNewPatientRequest) (err error) {
 
-	param := &model.MstPatientInstitution{
-		DateOfBirth: req.DateOfBirth,
-	}
-	if len(req.Name) > 0 {
-		param.Name = sql.NullString{
-			String: req.Name,
-			Valid:  true,
-		}
-	}
-
-	err = u.PatientDB.RegisterNewPatient(ctx, param) // &model.MstPatientInstitution{
-	// NIK:          req.NIK,
-	// Name:         req.Name,
-	// DateOfBirth:  req.DateOfBirth,
-	// PlaceOfBirth: req.PlaceOfBirth,
-	// Address:      req.Address,
-	// Religion:     req.Religion,
-	// }
+	err = u.PatientDB.RegisterNewPatient(ctx, &model.MstPatientInstitution{
+		NIK:          req.NIK,
+		Name:         req.Name,
+		DateOfBirth:  req.DateOfBirth,
+		PlaceOfBirth: req.PlaceOfBirth,
+		Address:      req.Address,
+		Religion:     req.Religion,
+	})
 
 	if err != nil {
 		err = errors.Wrap(err, WrapMsgRegisterNewPatient)
