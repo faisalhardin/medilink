2845760ddb9c34aed7bc772dc2f9a651608ce334
faisalhardin <rangkuti.hardin@gmail.com>
2026-05-17 15:21:29 +0700

    feat: add concurrent

--- schema/medianne/20260516_add_idx_visit_product_institution_time.sql
+++ schema/medianne/20260516_add_idx_visit_product_institution_time.sql
@@ -1,4 +1,4 @@
 -- Speed up institution-scoped product order statistics queries.
-CREATE INDEX IF NOT EXISTS idx_trx_visit_product_institution_create_time
+CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_trx_visit_product_institution_create_time
     ON mdl_trx_visit_product (id_mst_institution, create_time)
     WHERE delete_time IS NULL;
